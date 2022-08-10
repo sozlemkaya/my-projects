31d4335b509425ff2265afd63fa87f7070b7933a
ozlem <siyabeozlem@gmail.com>
2022-08-10 22:41:58 +0300

    flask-01 and flask-02 added

--- flask-01-hello-world-app/hello-world-app.py
+++ flask-01-hello-world-app/hello-world-app.py
@@ -9,6 +9,19 @@ def hello():
     return "Hello World from Flask!!!"
 
 
+@app.route('/second')
+def second():
+    return 'My Second Line from Flask!!!!'
+
+@app.route('/third/subthird')
+def third():
+    return 'This is the subpage of third page'
+
+
+@app.route('/forth/<string:id>')
+def forth(id):
+    return f'Id number of this page is {id}'
+
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, port=2000)
